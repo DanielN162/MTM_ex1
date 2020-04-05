e209204c96f2a4e1f5c0a6ea63518446672a56d9
Daniel Noor <63052638+DanielN162@users.noreply.github.com>
2020-04-05 14:10:32 +0300

    Compiled a C program as a test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/danie/Documents/GitHub/MTM_ex1
+# Build directory: C:/Users/danie/Documents/GitHub/MTM_ex1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
