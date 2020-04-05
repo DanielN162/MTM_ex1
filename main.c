178d96b44265c253c0565f13c25e08dadd02dcd5
Daniel Noor <63052638+DanielN162@users.noreply.github.com>
2020-04-05 14:43:20 +0300

    Initial Testing

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!");
+void printArray(char const **strings, int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%s\n", strings[i]);
+  }
 }
+
+int main(int argc, char const *argv[]) {
+  char const *strings[4] = {"hello to you", "and", "everyone", "else"};
+  printArray(strings, 4);
+  /* printArray(argv + 1, argc - 1); */
+  return 0;
+}
+
